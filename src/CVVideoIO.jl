e7430ffcd84f928f18e931e6ca8dbdd80b26bbf9
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-16 16:05:44 +0900

    moved from opencv.jl

--- src/CVVideoIO.jl
+++ src/CVVideoIO.jl
@@ -1,5 +1,34 @@
 module CVVideoIO
 
-# package code goes here
+export VideoCapture, read, release
+
+using LibOpenCV
+using CVCore
+using Cxx
+
+libopencv_videoio = LibOpenCV.find_library_e("libopencv_videoio")
+try
+    Libdl.dlopen(libopencv_videoio, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
+
+cxx"""
+#include <opencv2/videoio.hpp>
+"""
+
+typealias VideoCapture cxxt"cv::VideoCapture"
+
+VideoCapture(idx::Int) = icxx"cv::VideoCapture($idx);"
+
+function Base.read(cap::VideoCapture)
+    img = Mat{UInt8}()
+    ok = icxx"cap->read(img.handle);"
+    return ok, Mat(img.handle)
+end
+
+release(cap::VideoCapture) = icxx"$cap->release();"
+
 
 end # module
